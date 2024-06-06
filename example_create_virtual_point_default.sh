dbadb2d8ef6f8fcdce49694d9d8bd5ff6a92a917
mdupays <malvina.dupays@ign.fr>
2024-06-06 10:52:20 +0200

    refacto script for lauching steps

---
+++ example_create_virtual_point_default.sh
@@ -0,0 +1,9 @@
+# For lauching Create virtual point
+python -m lidro.main_create_virtual_point \
+io.input_dir=./data/ \
+io.input_mask_hydro=./data/merge_mask_hydro/MaskHydro_merge.geosjon \
+io.input_skeleton=./data/skeleton_hydro/skeleton_hydro.geojson \
+io.output_dir=./tmp/ \
+
+
+
