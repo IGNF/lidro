2c0e6a29829e7b081d81af510bce219ae3973672
mdupays <malvina.dupays@ign.fr>
2024-07-11 11:20:19 +0200

    rectify function's comments

--- lidro/create_virtual_point/stats/calculate_stat.py
+++ lidro/create_virtual_point/stats/calculate_stat.py
@@ -3,9 +3,9 @@
 import numpy as np
 
 
-def calculate_quartile(points: np.array, q: int) -> float:
+def calculate_quartile_(points: np.array, q: int) -> float:
     """
-    Calculates the quartile of the value (ex. altitude from z-coordinate) of points.
+    Calculates the quartile's value of Z coordinates
 
     Parameters:
         - points (numpy.ndarray): An array of 3D points [x, y, z]
@@ -13,25 +13,25 @@ def calculate_quartile(points: np.array, q: int) -> float:
                    Values must be between 0 and 100 inclusive.
 
     Returns:
-        - float: The quartile of the value (ex. z-coordinates).
+        - float: The quartile of Z coordinates
     """
     altitudes = points[:, 2]  # Extract the altitude column
-    n_quartile = round(np.percentile(altitudes, q), 2)
+    n_quartile = np.round(np.percentile(altitudes, q), 2)
 
     return n_quartile
 
 
 def calculate_median(points: np.array) -> float:
     """
-    Calculates the median of the value (ex. altitude from z-coordinate) of points.
+    Calculates the median's value of Z coordinates
 
     Parameters:
         - points (numpy.ndarray): An array of 3D points [x, y, z]
 
     Returns:
-        - float: The median of the value (ex. z-coordinates).
+        - float: The median of Z coordinates
     """
     altitudes = points[:, 2]  # Extract the altitude column
-    n_median = round(np.median(altitudes), 2)
+    n_median = np.round(np.median(altitudes), 2)
 
     return n_median
