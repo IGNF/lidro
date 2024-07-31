c089b73f27f787c511c2e43b2fce377145576433
mdupays <malvina.dupays@ign.fr>
2024-07-31 17:17:01 +0200

    add function for applying Z from 2D grid

---
+++ lidro/create_virtual_point/vectors/apply_Z_from_grid.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+""" Apply Z from grid
+"""
+import geopandas as gpd
+from shapely import line_locate_point
+
+
+def calculate_grid_z_with_model(points: gpd.GeoDataFrame, line: gpd.GeoDataFrame, model) -> gpd.GeoDataFrame:
+    """Calculate Z with regression model
+
+    Args:
+        points (gpd.GeoDataFrame): A GeoDataFrame containing the grid points
+        line (gpd.GeoDataFrame): A GeoDataFrame containing each line from Hydro's Skeleton
+        model (dict): A dictionary representing the regression model
+
+    Returns:
+        gpd.GeoDataFrame: A GeoDataFrame of initial points, but with a Z.
+    """
+    # Calculate curvilinear abscises for all points of the grid
+    curvilinear_abs = line_locate_point(line.loc[0, "geometry"], points["geometry"].array, normalized=False)
+
+    # Prediction of Z values using the regression model
+    # Its possible to use non-linear models for prediction
+    predicted_z = model(curvilinear_abs)
+
+    # Generate a new geodataframe, with 3D points
+    grid_with_z = gpd.GeoDataFrame(
+        geometry=gpd.GeoSeries().from_xy(points.geometry.x, points.geometry.y, predicted_z), crs=points.crs
+    )
+
+    return grid_with_z
+
+
+def calculate_grid_z_for_flattening(
+    points: gpd.GeoDataFrame, line: gpd.GeoDataFrame, predicted_z: float
+) -> gpd.GeoDataFrame:
+    """Calculate Z for flattening
+
+    Args:
+        points (gpd.GeoDataFrame): A GeoDataFrame containing the grid points
+        line (gpd.GeoDataFrame): A GeoDataFrame containing each line from Hydro's Skeleton
+        predicted_z (float): predicted Z for flattening river
+
+    Returns:
+        gpd.GeoDataFrame: A GeoDataFrame of initial points, but with a Z.
+    """
+    # Generate a new geodataframe, with 3D points
+    grid_with_z = gpd.GeoDataFrame(
+        geometry=gpd.GeoSeries().from_xy(points.geometry.x, points.geometry.y, predicted_z), crs=points.crs
+    )
+
+    return grid_with_z
