7aaf6067545451b530ffc1e6263e8c9493908821
mdupays <malvina.dupays@ign.fr>
2024-08-09 15:45:45 +0200

    refacto test for flattening river

--- test/vectors/test_flatten_river.py
+++ test/vectors/test_flatten_river.py
@@ -10,7 +10,7 @@ def test_flatten_little_river_points_knn_not_empty():
     points = gpd.GeoDataFrame(
         {
             "geometry": [Point(0, 0, 10), Point(1, 1, 20), Point(2, 2, 30)],
-            "points_knn": [np.array([[0, 0, 10], [1, 1, 20], [2, 2, 30]])],
+            "points_knn": [np.array([[0, 0, 10], [1, 1, 20], [2, 2, 30]]) for _ in range(3)],
         }
     )
 
@@ -18,7 +18,7 @@ def test_flatten_little_river_points_knn_not_empty():
     line = gpd.GeoDataFrame({"geometry": [LineString([(0, 0), (2, 2)])]})
 
     # Test when points_knn is not empty
-    expected_result = 10
+    expected_result = 15.0  # Adjusted to the correct 1st quartile value
     result = flatten_little_river(points, line)
     assert result == expected_result, f"Expected {expected_result}, but got {result}"
 
@@ -40,7 +40,7 @@ def test_flatten_little_river_duplicate_points():
     duplicate_points = gpd.GeoDataFrame(
         {
             "geometry": [Point(0, 0, 10), Point(1, 1, 10), Point(2, 2, 10)],
-            "points_knn": [np.array([[0, 0, 10], [1, 1, 10], [2, 2, 10]])],
+            "points_knn": [np.array([[0, 0, 10], [1, 1, 10], [2, 2, 10]]) for _ in range(3)],
         }
     )
 
