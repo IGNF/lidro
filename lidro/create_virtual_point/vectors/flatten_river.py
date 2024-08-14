1c18bc2f364387f8fcb2104e3d4e93f42e31b813
mdupays <malvina.dupays@ign.fr>
2024-08-14 11:00:35 +0200

    update flatten river for avoid error

--- lidro/create_virtual_point/vectors/flatten_river.py
+++ lidro/create_virtual_point/vectors/flatten_river.py
@@ -24,15 +24,28 @@ def flatten_little_river(points: gpd.GeoDataFrame, line: gpd.GeoDataFrame):
     """
     # Inputs
     gdf_points = return_points_by_line(points, line)
-    # Merge points and remove duplicates
-    all_points_knn = np.vstack(gdf_points["points_knn"].dropna().values)
-    if all_points_knn.size == 0:
-        logging.warning("For little river : error Z quartile")
+
+    # Extract points_knn and drop NaNs
+    points_knn_values = gdf_points["points_knn"].dropna().values
+
+    # Check if points_knn_values is empty
+    if points_knn_values.size == 0:
+        logging.warning("For little river : no valid points found, returning default Z quartile as 0")
         z_first_quartile = 0
-    else:
-        unique_points_knn = np.unique(all_points_knn, axis=0)
-        # Calculate the 1st quartile of all points
-        first_quartile = np.percentile(unique_points_knn, 25, axis=0)
-        z_first_quartile = first_quartile[-1]
+        return z_first_quartile
+
+    # Merge points and remove duplicates
+    try:
+        all_points_knn = np.vstack(points_knn_values)
+    except ValueError as e:
+        logging.error(f"Error during stacking points: {e}")
+        z_first_quartile = 0
+        return z_first_quartile
+
+    unique_points_knn = np.unique(all_points_knn, axis=0)
+
+    # Calculate the 1st quartile of all points
+    first_quartile = np.percentile(unique_points_knn, 25, axis=0)
+    z_first_quartile = first_quartile[-1]
 
     return z_first_quartile
