1b90dc198b69b634fe76d885d5724ca7f9173ac4
mdupays <malvina.dupays@ign.fr>
2024-09-25 14:01:40 +0200

    refacto function for compute virtual point by tile

--- test/test_main_clip_virtual_point_by_tile.py
+++ test/test_main_clip_virtual_point_by_tile.py
@@ -4,28 +4,31 @@ from pathlib import Path
 
 from hydra import compose, initialize
 
-from lidro.main_create_virtual_point import main
+from lidro.main_clip_virtual_point_by_tile import main
 
 INPUT_DIR = Path("data/pointcloud")
+INPUT_DIR_POINT_VIRTUAL = Path("data/point_virtual")
 OUTPUT_DIR = Path("tmp") / "clip_virtual_point_by_tile/main"
 
 
 def setup_module(module):
-    os.makedirs("tmp/create_virtual_point/main", exist_ok=True)
+    os.makedirs("tmp/clip_virtual_point_by_tile/main", exist_ok=True)
 
 
 def test_main_run_okay():
     repo_dir = Path.cwd().parent
     cmd = f"""python -m lidro.main_clip_virtual_point_by_tile \
         io.input_dir="{repo_dir}/lidro/data/pointcloud/"\
+        io.input_dir_point_virtual="{repo_dir}/lidro/data/point_virtual/"\
         io.output_dir="{repo_dir}/lidro/tmp/clip_virtual_point_by_tile/main/"
         """
     sp.run(cmd, shell=True, check=True)
 
 
-def test_main_lidro_input_file():
+def test_main_clip_virtual_point_by_tile():
     input_dir = INPUT_DIR
-    output_dir = OUTPUT_DIR / "main_lidro_input_file"
+    input_dir_point_virtual = INPUT_DIR_POINT_VIRTUAL
+    output_dir = OUTPUT_DIR / "main_clip_virtual_point_by_tile"
     input_filename = "Semis_2021_0830_6291_LA93_IGN69.laz"
     srid = 2154
 
@@ -36,9 +39,11 @@ def test_main_lidro_input_file():
             overrides=[
                 f"io.input_filename={input_filename}",
                 f"io.input_dir={input_dir}",
+                f"io.input_dir_point_virtual={input_dir_point_virtual}",
                 f"io.output_dir={output_dir}",
                 f"io.srid={srid}",
             ],
         )
     main(cfg)
     assert (Path(output_dir) / "tiles_from_las.GeoJSON").is_file()
+    assert (Path(output_dir) / input_filename).is_file()
