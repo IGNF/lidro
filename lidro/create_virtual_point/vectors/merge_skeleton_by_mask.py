35fdde109c706e06a733d1b68987d6c2d80187e0
Lea Vauchier <lea.vauchier@ign.fr>
2024-10-17 18:05:02 +0200

    WIP: Fix self-crossing skeletons

--- lidro/create_virtual_point/vectors/merge_skeleton_by_mask.py
+++ lidro/create_virtual_point/vectors/merge_skeleton_by_mask.py
@@ -6,7 +6,9 @@ import os
 
 import geopandas as gpd
 import pandas as pd
-from shapely.geometry import LineString, MultiLineString
+from shapely import line_merge
+
+from lidro.vectors.close_holes import close_holes
 
 
 def explode_multipart(gdf: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
@@ -24,62 +26,75 @@ def explode_multipart(gdf: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
     return exploded_geom
 
 
-def combine_and_connect_lines(geometries: list) -> LineString:
-    """Combines and connects a list of LineString and MultiLineString geometries into a single LineString.
+def combine_skeletons(gdf_skeleton: gpd.GeoDataFrame, gdf_mask_hydro: gpd.GeoDataFrame, crs: str) -> gpd.GeoDataFrame:
+    """Combine skeletons that belong to the same entity
+    (single polygon) of the hydro mask
 
     Args:
-        geometries (list): A list of LineString and/or MultiLineString objects.
+        gdf_skeleton (gpd.GeoDataFrame): hydro skeletons (one skeleton by row)
+        gdf_mask_hydro (gpd.GeoDataFrame): hydro mask (one entity/polygon by row)
+        crs (str): a pyproj CRS object used to create the output
+
+    Raises:
+        ValueError: when some skeleton lines are disjoint inside a single mask polygon
 
     Returns:
-        LineString: The merged and connected geometry.
+        gpd.GeoDataFrame: each row contains the combined skeleton lines for the one hydro entity
     """
-    # Convert all geometries into lists of lines
-    lines = [geom.geoms if isinstance(geom, MultiLineString) else [geom] for geom in geometries]
-    # Extract and combine all coordinates from the nested list using a list comprehension
-    all_coords = [
-        coord for sublist in lines for line in sublist if isinstance(line, LineString) for coord in line.coords
-    ]
-    # Create a new connected line
-    connected_line = LineString(all_coords)
-
-    return connected_line
-
-
-def merge_skeleton_by_mask(
-    input_skeleton: gpd.GeoDataFrame, input_mask_hydro: gpd.GeoDataFrame, output_dir: str, crs: str
-) -> pd.DataFrame:
-    """Combine skeleton lines into a single polyline for each hydro entity.
-        Save a invalid mask to GeoDataframe
-
-    Args:
-        input_skeleton (gpd.GeoDataFrame): A GeoDataFrame containing each line from Hydro's Skeleton
-        input_mask_hydro (gpd.GeoDataFrame): A GeoDataFrame containing Mask Hydro
-        output_dir (str): output folder
-        crs (str): a pyproj CRS object used to create the out
-
-    Returns:
-       Dataframe :  Dataframe with single polyline "skeleton" by hydro entity
-    """  # Inputs
-    gdf_skeleton = gpd.read_file(input_skeleton)
-    gdf_skeleton = explode_multipart(gdf_skeleton)
-    gdf_mask_hydro = gpd.read_file(input_mask_hydro)
-    gdf_mask_hydro = explode_multipart(gdf_mask_hydro)
-
     # # Perform a spatial join to find skeletons within each mask_hydro
     gdf_joined = gpd.sjoin(gdf_skeleton, gdf_mask_hydro, how="inner", predicate="intersects")
-    # geometry intersections with funtion "overlay"
+    # geometry intersections with function "overlay"
     gdf_intersections = gpd.overlay(gdf_joined, gdf_mask_hydro, how="intersection", keep_geom_type=True)
 
     # Combine skeleton lines into a single polyline for each hydro entity
-    combined_skeletons = gdf_intersections.groupby("index_right")["geometry"].apply(combine_and_connect_lines)
+    # Group geometries based on index_right
+    combined_skeletons = gdf_intersections.dissolve(by="index_right")
+    # Apply line_merge where it is possible (skeleton lines are joint), raise an error in other cases
+    combined_skeletons["geometry"] = combined_skeletons["geometry"].apply(line_merge)
+
+    # if not (combined_skeletons["geometry"].geom_type == "LineString").all() :
+    #     raise ValueError("Combined skeleton geometry should all be of type LineString,"
+    #                     f"got {combined_skeletons['geometry'].geom_type} instead. This means that"
+    #                     "there is a hole in the skeleton inside one hydro entity")
+
     gdf_combined_skeletons = gpd.GeoDataFrame(combined_skeletons, columns=["geometry"], crs=crs).reset_index()
+
     # # Re-join with mask_hydro to keep only the combined skeletons within masks
     gdf_joined_combined = gpd.sjoin(
         gdf_combined_skeletons, gdf_mask_hydro, how="inner", predicate="intersects", lsuffix="combined", rsuffix="mask"
     )
 
+    return gdf_joined_combined
+
+
+def merge_skeleton_by_mask(input_skeleton: str, input_mask_hydro: str, output_dir: str, crs: str) -> pd.DataFrame:
+    """Combine skeleton lines into a single polyline for each hydro entity.
+        Save a invalid mask to GeoDataframe
+
+    Args:
+        input_skeleton (str): the path to a file (that can be converted to a GeoDataFrame)
+            containing each line from Hydro's Skeleton
+        input_mask_hydro (str): he path to a file (that can be converted to a GeoDataFrame)
+            containing the hydro mask
+        output_dir (str): output folder
+        crs (str): a pyproj CRS object used to create the out
+
+    Returns:
+       Dataframe :  Dataframe with single "skeleton" LineString by hydro entity
+    """
+    # Inputs
+    gdf_skeleton = gpd.read_file(input_skeleton)
+    gdf_skeleton = explode_multipart(gdf_skeleton)
+
+    gdf_mask_hydro = gpd.read_file(input_mask_hydro)
+    gdf_mask_hydro = explode_multipart(gdf_mask_hydro)
+    # Close holes in gdf_mask-hydro to prevent island from dividing a skeleton into multiple parts
+    gdf_mask_hydro["geometry"] = gdf_mask_hydro["geometry"].apply(close_holes)
+
+    gdf_combined = combine_skeletons(gdf_skeleton, gdf_mask_hydro, crs)
+
     # # Count the number of skeletons per mask
-    skeleton_counts = gdf_joined_combined["index_mask"].value_counts()
+    skeleton_counts = gdf_combined["index_mask"].value_counts()
 
     valid_masks = skeleton_counts[skeleton_counts == 1].index
     invalid_masks = skeleton_counts[skeleton_counts != 1].index
@@ -87,7 +102,7 @@ def merge_skeleton_by_mask(
     # Filter to keep only masks with a single skeleton
     if not valid_masks.empty:
         # Filter the joined GeoDataFrame to keep only valid masks
-        gdf_valid_joined = gdf_joined_combined[gdf_joined_combined["index_mask"].isin(valid_masks)]
+        gdf_valid_joined = gdf_combined[gdf_combined["index_mask"].isin(valid_masks)]
         # Merge the geometries of masks and skeletons into the resulting GeoDataFrame
         df_result = gdf_valid_joined.merge(
             gdf_mask_hydro, left_on="index_mask", right_index=True, suffixes=("_skeleton", "_mask")
@@ -95,9 +110,12 @@ def merge_skeleton_by_mask(
         # Keep only necessary columns
         df_result = df_result[["geometry_skeleton", "geometry_mask"]]
 
+    else:
+        df_result = gpd.GeoDataFrame(columns=["geometry_skeleton", "geometry_mask"])
+
     if not invalid_masks.empty:
         # Filter the joined GeoDataFrame to keep invalid masks
-        gdf_invalid_joined = gdf_joined_combined[gdf_joined_combined["index_mask"].isin(valid_masks)]
+        gdf_invalid_joined = gdf_combined[gdf_combined["index_mask"].isin(valid_masks)]
         # Merge the geometries of masks and skeletons into the resulting GeoDataFrames
         df_exclusion = gdf_invalid_joined.merge(
             gdf_mask_hydro, left_on="index_mask", right_index=True, suffixes=("_skeleton", "_mask")
