55222d84fec51886c6c615cf31418a77e92e0f11
Lea Vauchier <lea.vauchier@ign.fr>
2024-10-24 09:32:47 +0200

    Fix path in tests

--- test/vectors/test_merge_vector.py
+++ test/vectors/test_merge_vector.py
@@ -11,7 +11,7 @@ from lidro.merge_mask_hydro.vectors.merge_vector import merge_geom
 TMP_PATH = Path("./tmp/merge_mask_hydro/vectors/merge_mask_hydro")
 
 input_folder = "./data/mask_hydro/"
-output = Path("./tmp/merge_mask_hydro/vectors/merge_mask_hydro/MaskHydro_merge_with_multibranch_skeleton.geojson")
+output = Path("./tmp/merge_mask_hydro/vectors/merge_mask_hydro/MaskHydro_merge.geojson")
 
 
 def setup_module(module):
